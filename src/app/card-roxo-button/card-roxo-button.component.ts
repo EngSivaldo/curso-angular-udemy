8f5b11b07bc8e7b0c868d3e099200b335b8726e5
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-09-06 19:28:21 -0300

    conceitos angula-components aula3 criando components filhos

---
+++ src/app/card-roxo-button/card-roxo-button.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-roxo-button',
+  templateUrl: './card-roxo-button.component.html',
+  styleUrl: './card-roxo-button.component.scss',
+})
+export class CardRoxoButtonComponent {}
+
+/*-
+-apos criar este componente filho
+venho aqui pagar selector ('app-card-roxo-button) e devo referenciar no card-roxo.comp.html
+- depois vou no card-roxo.comp.scss remover a class que cria button
+-e apos isso devo adiciona-la no card-roxo-button.comp.scss
+-e para finalizar vou no html do card-roxo-button criar uma div com a class criada acima e inserir o texto(Adquirir)
+*/
