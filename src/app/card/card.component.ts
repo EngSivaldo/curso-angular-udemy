7a05843a1f0aa84cf8788ad747f357be7946b643
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-09-07 19:08:50 -0300

    conceitos angular  Utilização da Interpolação

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  selector: 'app-card', // seletor usado no HTML (<app-card>)
+  templateUrl: './card.component.html', // arquivo HTML do template
+  styleUrls: ['./card.component.scss'], // ⚠️ aqui deveria ser "styleUrls" (plural) → senão o Angular não reconhece
 })
 export class CardComponent {
-
+  plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 1500,
+    },
+  };
 }
